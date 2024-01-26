f9ff7fe35cfd8e8c723a9807392651087931e07a
Kiarash Rahmani <microkia1928@gmail.com>
2024-01-27 01:41:30 +0330

    Adding some validations to my validation.js

--- JS/validating.js
+++ JS/validating.js
@@ -10,6 +10,14 @@
             var email = document.getElementById("Email").value;
             var maritalStatus = document.getElementById("Marital").value;
             var password = document.getElementById("Password").value;
+            var URL = document.getElementById("URL").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            var date = document.getElementById("date").value;
+            var ipAddress = document.getElementById("ipAddress").value;
+            var cardNumber = document.getElementById("cardNumber").value;
+            var username = document.getElementById("username").value;
+            var hexColor = document.getElementById("hexColor").value;
+            var zipCode = document.getElementById("zipCode").value;
 
 
             // Basic validation, you can add more specific checks as needed
@@ -18,12 +26,38 @@
             }
             else if (!isStrongPassword(password)) {
                  alert("Please enter a stronger password");
-            }               
+            }
+            else if (!isValidURL(URL)) {
+                 alert("Please enter a valid URL");
+            }
+            else if (!isValidPhoneNumber(phoneNumber)) {
+                 alert("Please enter a valid Phone number");
+            } 
+            else if (!isValidDate(date)) {
+                 alert("Please enter a valid Date");
+            } 
+            else if (!isValidIPAddress(ipAddress)) {
+                 alert("Please enter a valid IPAddress");
+            } 
+            else if (!isValidCreditCardNumber(cardNumber)) {
+                 alert("Please enter a valid CreditCardNumber");
+            } 
+            else if (!isValidUsername(username)) {
+                 alert("Please enter a valid Username");
+            } 
+            else if (!isValidHexColorCode(hexColor)) {
+                 alert("Please enter a valid HexColorCode");
+            }
+            else if (!isValidZIPCode(zipCode)) {
+                 alert("Please enter a valid ZIPCode");
+            }      
  
             else {
                 // Form is valid, you can proceed with form submission or other actions
-                alert("Form submitted successfully!\nFirstname: " + firstname + "\nLastname: " + lastname + "\nEmail: " + email + "\nMarital Status: " + maritalStatus );
-                // Add further processing or form submission logic here
+                alert("Form submitted successfully!\nFirstname: " + firstname + "\nLastname: "
+                    + lastname + "\nEmail: " + email + "\nMarital Status: " + maritalStatus + "\nURL:" + URL +
+                    "\nPhoneNumber:" + phoneNumber + "\nDate:" + date + "\nIPAddress:" + ipAddress + "\ncardNumber:" + cardNumber +
+                    "\nusername:" + username +"\nhexColor:"+ hexColor +"\nZipcode:" +zipCode);
             }
         }
 
@@ -59,5 +93,5 @@
             return /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(hexColor);
         }
         function isValidZIPCode(zipCode) {
-            return /^\d{5}(-\d{4})?$/.test(zipCode);
+            return /^\d{10}$/.test(zipCode);
         }

c2c0e2b0a3048b079eaffc477dec5f3079998592
Kiarash Rahmani <microkia1928@gmail.com>
2024-02-07 21:54:11 +0330

    updating some files

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.use(express.static(__dirname));
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-const getForm = require(path.join(__dirname, 'Public', 'JS', 'API.js'));
 
-// Handle requests to the root path '/'
+// Serve the form.html file at the root path
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'form.html'));
 });
 
+// Import route.js file
+const routes = require('./route');
+
+// Route handling
+app.use('/', routes);
+
+// Server
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
-
-app.use('/save', getForm);
-
